584a63be719dc6c22ec29f330a6e064a0cb17ee5
Reginald Osano <regie30developer@gmail.com>
2022-06-15 01:48:09 +0800

    Updated pushed 6-15-2022 @1:48am

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from  '@apollo/client';
+
+import Clients from './components/Clients';
+import AddClientModal from './components/AddClientModal';
+
+const cache = new InMemoryCache({
+	typePolicies: {
+		Query: {
+			fields: {
+				clients: {
+					merge(existing, incoming) {
+						return incoming;
+					}
+				},
+				projects: {
+					merge(existing, incoming) {
+						return incoming;
+					}
+				}
+			}
+		}
+	}
+});
+
+const client = new ApolloClient({
+	 uri: 'http://localhost:6500/graphql',
+	 cache, 
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+		  <ApolloProvider client={client}>
+		  	<Header />
+		    <div className="container">
+					<AddClientModal />
+		      <Clients />
+	    	</div>
+			</ApolloProvider>
+		 
+		</>
+		
+
+	);
 }
 
 export default App;
